84e4e2bdab0ac86435a7b2699825b2273b55f04b
enrique <enriqueadhemar@gmail.com>
2023-09-16 12:03:53 -0400

    test de integracion y jenkins

---
+++ frontend/cypress/e2e/3-calculator/operations.cy.js
@@ -0,0 +1,37 @@
+/// <reference types="cypress" />
+
+describe('example to-do app', () => {
+    beforeEach(() => {
+
+      cy.visit('http://localhost:5173/')
+    })
+  
+    it('Muestra el componente calculadora', () => {
+
+      cy.get('#operation').should('exist').and("have.value", "0");
+  
+    })
+  
+    it('Realiza una suma correctamente', () => {
+      cy.contains('8').should('exist').click();
+      cy.contains('+').should('exist').click();
+      cy.contains('4').should('exist').click();
+      cy.contains('2').should('exist').click();
+      cy.contains('=').should('exist').click();
+
+      cy.get('#operation').should("have.value", "=50");
+
+    })
+
+    it('Verificar error de sintaxis', () => {
+        cy.contains('1').should('exist').click();
+        cy.contains('6').should('exist').click();
+        cy.contains('4').should('exist').click();
+        cy.contains('/').should('exist').click();
+        cy.contains('=').should('exist').click();
+  
+        cy.get('#operation').should("have.value", "Syntax Error");
+  
+      })
+  })
+  
